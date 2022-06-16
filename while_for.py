773b4d22ad3af91c30cc57b198b1a16f317eebae
JuanC0097 <96270504+JuanC0097@users.noreply.github.com>
2022-06-16 14:12:00 -0500

    Creacion del ejercicio While_For
    Se mostrara por medio de tres ejercicios con funciona el ciclo while y el ciclo for, impelementando Break y Continue

--- while_for.py
+++ while_for.py
@@ -1,16 +1,17 @@
 def run():
+   #EJERCICIO 1
    # i = 0
    # while i < 100:
    #     i += 1
    #     print(i)
    #     if i == 87:
    #         break
-   
+   #EJERCICIO 2
    #for contador in range(1000):
     #    if contador % 2 != 0:
     #        continue
     #    print(contador)
-
+   #Ejercicio 3
     contador = 0
     while contador < 10:
         contador += 1
