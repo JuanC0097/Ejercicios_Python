a367c012e986630908e117ffbb012a960d90ec43
JuanC0097 <96270504+JuanC0097@users.noreply.github.com>
2022-06-16 14:05:08 -0500

    Creacion del ejercicio generar contraseñas
    Por medio de dos funciones, una libreria y un bucle for se realiza un programa que generara una contraseña ramdon de cierta lista de caracteres.

--- generador_contrasena.py
+++ generador_contrasena.py
@@ -1,16 +1,16 @@
 import random
 
-
+#Se utiliza una funcion que guardara 4 listas de caracteres
 def generar_contraseña():
     mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     minusculas = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     simbolos = ["@","?","¿","!","$","%","&","/","(","=","}","{","_","-","*","+",":",".",",",";","[","`"]
     numero = ["1","2","3","4","5","6","7","8","9","0"]
-
+    #Se establece una variable que guardara las listas anteriores
     caracteres = mayusculas + minusculas + simbolos + numero
-
+    #Se crea una lista vacia que mas adelante albergara la contraseña
     contraseña = []
-
+    #Por medio del bucle for, se crea una variable que guardara los caracteres ramdom de la lista caracteres
     for i in range(15):
         caracter_ramdom = random.choice(caracteres)
         contraseña.append(caracter_ramdom)
