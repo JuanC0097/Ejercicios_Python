e4e41cc2fd39ab71c3517f0a610056901d79497e
JuanC0097 <96270504+JuanC0097@users.noreply.github.com>
2022-06-16 13:56:48 -0500

    Creacion del ejercicio for
    Ejercicio basico con bucle for para contar el 1 al 1000

--- for.py
+++ for.py
@@ -1,3 +1,3 @@
-#Bucle en el cual el contador tomara el valor del rango,cada vez que se repita
+#Bucle en el cual el contador toma el valor del rango,cada vez que se repita
 for contador in range(1,1001):
     print(contador)
