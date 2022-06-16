e2a23898f3ed312f74a8e5f3daec24b7a6ede819
JuanC0097 <96270504+JuanC0097@users.noreply.github.com>
2022-06-16 13:35:58 -0500

    Creacion del ejercicio Recorrer
    Utilice una funcion la cual posee un ciclo for encargada de realizar el recorrido de la varaible frase y imprime cada uno de los caracteres

--- Recorrer.py
+++ Recorrer.py
@@ -1,8 +1,9 @@
 def run():
+    #1 forma
     #nombre= input("Escribe tu nombre: ")
     #for letra in nombre:
     #    print(letra)
-
+    #Segunda forma
     frase = input("Escribe una frase: ")
     for caracter in frase:
         print(caracter.upper())
